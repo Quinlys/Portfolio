b05b1f20413bb549c589e8dd7c5c3bfb37ecf0c5
Igor <yakymiv.ig@gmail.com>
2019-09-18 18:07:29 +0300

    Upload project from hackathon 30hack

---
+++ OnlineDekanat, hackathon, frontend/DovidkaD/index.js
@@ -0,0 +1,15 @@
+
+document.getElementById('delete1').onclick = () => document.getElementById('certificate1').remove();
+document.getElementById('delete2').onclick = () => document.getElementById('certificate2').remove();
+document.getElementById('delete3').onclick = () => document.getElementById('certificate3').remove();
+document.getElementById('delete4').onclick = () => document.getElementById('certificate4').remove();
+document.getElementById('delete5').onclick = () => document.getElementById('certificate5').remove();
+document.getElementById('btn1').onclick = () => document.getElementById('certificate1').remove();
+document.getElementById('btn2').onclick = () => document.getElementById('certificate2').remove();
+document.getElementById('btn3').onclick = () => document.getElementById('certificate3').remove();
+document.getElementById('btn4').onclick = () => document.getElementById('certificate4').remove();
+document.getElementById('btn5').onclick = () => document.getElementById('certificate5').remove();
+
+
+
+
